67be6a397ec67c19954bf881bfdab295cba99020
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-07-22 13:14:44 +0700

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ loadComponent("header", "components/header.html");
 loadComponent("section-1", "components/section-1.html");
 loadComponent("section-2", "components/sestion-2.html");
 loadComponent("section-3", "components/sestion-3.html");
+loadComponent("section-4", "components/sestion-4.html");
 loadComponent("footer", "components/footer.html");
 
 
